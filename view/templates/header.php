7597fd67419dd6ed842e45208239defca8bbf08a
Peter Snoek <petersnoek@davinci.nl>
2017-06-10 12:07:12 +0200

    show errors from session, and show a 'login' <> 'logout' link in menu (depending if user is logged in)

--- view/templates/header.php
+++ view/templates/header.php
@@ -10,7 +10,29 @@
 <body>
 	<nav>
 	<ul>
+        <?php // show 'login' link (if user not logged in) or 'logout' link (if user is logged in)
+        if (isset($_SESSION['username'])) { ?>
+            <li><a href="<?= URL ?>user/logout"><?= $_SESSION['username'] ?> afmelden</a></li>
+        <?php } else { ?>
+            <li><a href="<?= URL ?>user/login">Inloggen</a></li>
+        <?php } ?>
+
 		<li><a href="<?= URL ?>home/index">Home</a></li>
 		<li><a href="<?= URL ?>student/index">Students</a></li>
 	</ul>
 	</nav>
+
+    <?php
+        // if errors found, print them
+        if (isset($_SESSION['errors']) && is_array($_SESSION['errors']) && sizeof($_SESSION['errors'])>0 ) {
+            echo '<div class="alert alert-danger"><strong>Fout!</strong> <ul>';
+            foreach($_SESSION['errors'] as $error) {
+                echo '<li>' . $error . '</li>';
+            }
+            echo '</ul></div>';
+
+            // errors are shown. now remove them from session
+            $_SESSION['errors'] = [];
+        }
+    ?>
+
